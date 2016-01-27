e5f929b6c389213b2863b2993c1e65a61c3f0764
mac33 <mac33@a5s104mac23.fib.upc.es>
2016-01-27 18:00:02 +0100

    First commit

---
+++ list/CellViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CellViewControllerTableViewCell.swift
+//  list
+//
+//  Created by mac33 on 27/1/16.
+//  Copyright © 2016 mac33. All rights reserved.
+//
+
+import UIKit
+
+class CellViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
